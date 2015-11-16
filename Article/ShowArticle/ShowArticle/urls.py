d7795bcd414c682a842c69b17a5bdc786162990d
persistforever <caocao7066@outlook.com>
2015-11-16 15:13:32 +0800

    del

--- Article/ShowArticle/ShowArticle/urls.py
+++
@@ -1,9 +0,0 @@
-﻿from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^', include('polls.urls', namespace="polls")),
-    url(r'^admin/', include(admin.site.urls)),
-)
