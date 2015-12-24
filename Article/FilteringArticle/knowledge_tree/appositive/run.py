e51cec9b9d6d9f97f14d61c3394f208f69ea907c
荣禹 曹 <caocao7066@outlook.com>
2015-12-24 19:22:12 +0800

    123

--- Article/FilteringArticle/knowledge_tree/appositive/run.py
+++ Article/FilteringArticle/knowledge_tree/appositive/run.py
@@ -19,7 +19,7 @@ class Corpus :
         self.run_find_appositive(sentences_path, wordembed_path, appositive_path)
 
     def run_find_appositive(self, sentences_path, wordembed_path, appositive_path) :
-        seed = u'裤'
+        seed = [u'裤', u'裙', u'衣', u'衫', u'服', u'鞋', u'靴', u'包']
         sentences = self.read_sentences(sentences_path)
         appositive = Appositive()
         word_dict = appositive.find_appositive(seed, sentences)
@@ -33,8 +33,12 @@ class Corpus :
         data_list = file_operator.reading(sentences_path)
         entry_list = data_list[0]
         sentences = list()
+        length = len(data_list[1:]) - 1
         for idx, data in enumerate(data_list[1:]) :
             if len(data) >= len(entry_list) :
                 sentence = [Word(word, sp_char=':') for word in data[0].split(' ')]
                 sentences.append(sentence)
-        return sentence
+            if idx % 100 == 0 :
+                print 'finish rate is %.2f%%\r' % (100.0*idx/length),
+        print 'finish rate is %.2f%%\r' % (100.0*idx/length)
+        return sentences
